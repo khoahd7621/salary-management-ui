16375fe2fd0e0f9cc0b226a45f94856bc6bb1bf8
khoahd7621 <hoangdangkhoa7621@gmail.com>
2023-02-10 19:07:22 +0700

    Setup ant design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
 import { AppProps } from 'next/app';
+import { MainLayout } from '~/components/common/Layout';
+
+import 'antd/dist/reset.css';
+import '~/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
+  );
 }
