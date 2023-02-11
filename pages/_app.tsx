bcac11bc98de0d45a79e37be033a9fd56ea14e3b
khoahd7621 <hoangdangkhoa7621@gmail.com>
2023-02-11 23:53:06 +0700

    Setup redux-toolkit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import { AppProps } from 'next/app';
 import { MainLayout } from '~/components/common';
+import { Provider } from 'react-redux';
+
+import { store } from '~/redux/store';
 
 import 'antd/dist/reset.css';
 import '~/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <Provider store={store}>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </Provider>
   );
 }
