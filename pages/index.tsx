e16a8da16aed7d623abec2a7cd9d20063b2bba49
khoahd7621 <hoangdangkhoa7621@gmail.com>
2023-02-12 22:53:09 +0700

    Support SEO for login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 import { SubLayout } from '~/components/layouts';
+import { Seo } from '~/components';
 import { NextPageWithLayout } from '~/models/components/layouts';
 import { LoginPayload } from '~/modules/login/models/loginPayload';
 import { login } from '~/modules/login/services/authService';
@@ -24,53 +25,63 @@ const LoginPage: NextPageWithLayout = () => {
   };
 
   return (
-    <div>
-      <div className="max-w-md mt-8 mx-auto p-4 bg-white border rounded-md border-solid border-gray-400">
-        <Typography.Title level={3} className="text-center !mb-8">
-          Login
-        </Typography.Title>
-        <Form
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={handleLogin}
-        >
-          <Space className="w-full" direction="vertical" size="small">
-            <Form.Item
-              name="username"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your Username!',
-                },
-              ]}
-            >
-              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-            </Form.Item>
-            <Form.Item
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your Password!',
-                },
-                {
-                  min: 6,
-                  message: 'Password must be at least 6 characters',
-                },
-              ]}
-            >
-              <Input.Password prefix={<LockOutlined className="site-form-item-icon" />} placeholder="Password" />
-            </Form.Item>
-            <Form.Item>
-              <Button disabled={isSending} type="primary" htmlType="submit" className="w-full">
-                Log in
-              </Button>
-            </Form.Item>
-          </Space>
-        </Form>
+    <>
+      <Seo
+        data={{
+          title: 'Login',
+          description: 'Login page',
+          url: `${process.env.HOST_URL}`,
+        }}
+      />
+
+      <div>
+        <div className="max-w-md mt-8 mx-auto p-4 bg-white border rounded-md border-solid border-gray-400">
+          <Typography.Title level={3} className="text-center !mb-8">
+            Login
+          </Typography.Title>
+          <Form
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={handleLogin}
+          >
+            <Space className="w-full" direction="vertical" size="small">
+              <Form.Item
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your Username!',
+                  },
+                ]}
+              >
+                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your Password!',
+                  },
+                  {
+                    min: 6,
+                    message: 'Password must be at least 6 characters',
+                  },
+                ]}
+              >
+                <Input.Password prefix={<LockOutlined className="site-form-item-icon" />} placeholder="Password" />
+              </Form.Item>
+              <Form.Item>
+                <Button disabled={isSending} type="primary" htmlType="submit" className="w-full">
+                  Log in
+                </Button>
+              </Form.Item>
+            </Space>
+          </Form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
