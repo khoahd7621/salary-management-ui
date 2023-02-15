c140d1e0afd22b7af27e6e085dec7002469b0356
khoahd7621 <hoangdangkhoa7621@gmail.com>
2023-02-15 16:37:31 +0700

    Custom 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,12 +2,32 @@ import Link from 'next/link';
 
 import { EmptyLayout } from '~/layouts';
 import { NextPageWithLayout } from '~/models/layouts';
+import styles from '~/styles/404.module.scss';
 
 const NotfoundPage: NextPageWithLayout = () => {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <Link href="/">Go back home</Link>
+    <div className={styles['notfound']}>
+      <p className={styles['p']}>
+        HTTP: <span>404</span>
+      </p>
+      <code className={styles['code']}>
+        <span>this_page</span>.<em>not_found</em> = true;
+      </code>
+      <code className={styles['code']}>
+        <span>if</span> (<b>you_spelt_it_wrong</b>) &#123;<span>try_again()</span>;&#125;
+      </code>
+      <code className={styles['code']}>
+        <span>
+          else if (<b>we_screwed_up</b>)
+        </span>{' '}
+        &#123;<em>alert</em>(<i>&quot;We&apos;re really sorry about that.&quot;</i>); <span>window</span>.
+        <em>location</em> = home;&#125;
+      </code>
+      <center>
+        <Link className={styles['a']} href="/">
+          HOME
+        </Link>
+      </center>
     </div>
   );
 };
