e8f5022ff38645d666157a8ac303aa8f9a0c7c39
khoahd7621 <hoangdangkhoa7621@gmail.com>
2023-02-24 16:45:39 +0700

    Holiday - Optimaize duplicate code

---
+++ components/modules/holidays/HolidayForm.tsx
@@ -0,0 +1,74 @@
+import { Button, DatePicker, Form, Input, Select, Space } from 'antd';
+import { FormInstance } from 'antd/lib/form/Form';
+import Link from 'next/link';
+import { AppRoutes } from '~/models/constants/Routes';
+
+export interface HolidayFormProps {
+  form: FormInstance<any> | undefined;
+  onFinish: (_values: any) => void;
+  button: 'Create' | 'Update';
+  isSending: boolean;
+}
+
+export function HolidayForm({ form, onFinish, button, isSending }: HolidayFormProps) {
+  const { RangePicker } = DatePicker;
+
+  return (
+    <Form
+      form={form}
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      autoComplete="off"
+    >
+      <Space style={{ width: '100%' }} direction="vertical">
+        <Form.Item label="Name" name="name" rules={[{ required: true, message: 'Please input holiday name!' }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Apply date"
+          name="applyDate"
+          rules={[{ required: true, message: 'Please input apply date!' }]}
+        >
+          <RangePicker format={'DD/MM/YYYY'} />
+        </Form.Item>
+        <Form.Item
+          label="Paid"
+          name="isPaid"
+          rules={[{ required: true, message: 'Please select pay or unpay in this holiday!' }]}
+        >
+          <Select
+            showSearch
+            allowClear
+            onClear={() => form?.setFieldsValue({ isPaid: '' })}
+            onChange={(value) => form?.setFieldsValue({ isPaid: value })}
+            filterOption={(input, option) => (option?.label ?? '').toLowerCase().includes(input.toLowerCase())}
+            options={[
+              {
+                value: false,
+                label: 'No',
+              },
+              {
+                value: true,
+                label: 'Yes',
+              },
+            ]}
+          />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button disabled={isSending} type="primary" htmlType="submit">
+            {button}
+          </Button>
+          <Link style={{ marginLeft: '16px' }} href={`/${AppRoutes.holidays}`} passHref>
+            <Button type="primary" danger>
+              Cancel
+            </Button>
+          </Link>
+        </Form.Item>
+      </Space>
+    </Form>
+  );
+}
--- pages/holidays/create.tsx
+++ pages/holidays/create.tsx
@@ -1,18 +1,17 @@
-import { Button, DatePicker, Form, Input, message, Select, Space, Typography } from 'antd';
+import { Form, message, Space, Typography } from 'antd';
 import { Dayjs } from 'dayjs';
 import getConfig from 'next/config';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 import { holidayApi } from '~/api-clients/modules/holiday-api';
 import { Seo } from '~/components';
+import { HolidayForm } from '~/components/modules/holidays';
 import { AppRoutes } from '~/models/constants/Routes';
 
 const { serverRuntimeConfig } = getConfig();
 
 export default function CreateHolidayPage() {
-  const { RangePicker } = DatePicker;
   const [form] = Form.useForm();
   const router = useRouter();
   const [loading, setLoading] = useState<boolean>(false);
@@ -48,62 +47,9 @@ export default function CreateHolidayPage() {
           <Typography.Title level={3}>Create new holiday</Typography.Title>
         </section>
 
-        <Form
-          form={form}
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          style={{ maxWidth: 600 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          autoComplete="off"
-        >
-          <Space style={{ width: '100%' }} direction="vertical">
-            <Form.Item label="Name" name="name" rules={[{ required: true, message: 'Please input holiday name!' }]}>
-              <Input />
-            </Form.Item>
-            <Form.Item
-              label="Apply date"
-              name="applyDate"
-              rules={[{ required: true, message: 'Please input apply date!' }]}
-            >
-              <RangePicker format={'DD/MM/YYYY'} />
-            </Form.Item>
-            <Form.Item
-              label="Paid"
-              name="isPaid"
-              rules={[{ required: true, message: 'Please select pay or unpay in this holiday!' }]}
-            >
-              <Select
-                showSearch
-                allowClear
-                onClear={() => form.setFieldsValue({ isPaid: '' })}
-                onChange={(value) => form.setFieldsValue({ isPaid: value })}
-                filterOption={(input, option) => (option?.label ?? '').toLowerCase().includes(input.toLowerCase())}
-                options={[
-                  {
-                    value: false,
-                    label: 'No',
-                  },
-                  {
-                    value: true,
-                    label: 'Yes',
-                  },
-                ]}
-              />
-            </Form.Item>
-
-            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-              <Button disabled={loading} type="primary" htmlType="submit">
-                Create
-              </Button>
-              <Link style={{ marginLeft: '16px' }} href={`/${AppRoutes.holidays}`} passHref>
-                <Button type="primary" danger>
-                  Cancel
-                </Button>
-              </Link>
-            </Form.Item>
-          </Space>
-        </Form>
+        <section>
+          <HolidayForm form={form} onFinish={onFinish} button="Create" isSending={loading} />
+        </section>
       </Space>
     </>
   );
