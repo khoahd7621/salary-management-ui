37af03fa029aefbfaf067fd9e0e504c2e8bd3cca
khoahd7621 <hoangdangkhoa7621@gmail.com>
2023-03-09 21:54:57 +0700

    Remove redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,8 @@
-import { Provider } from 'react-redux';
 import { SWRConfig } from 'swr/_internal';
 
 import axiosClient from '~/api-clients/axios-client';
 import { MainLayout } from '~/layouts';
 import { AppPropsWithLayout } from '~/models/layouts';
-import { store } from '~/redux/store';
 
 import 'antd/dist/reset.css';
 import '~/styles/globals.scss';
@@ -13,17 +11,15 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const Layout = Component.Layout ?? MainLayout;
 
   return (
-    <Provider store={store}>
-      <SWRConfig
-        value={{
-          fetcher: (url) => axiosClient.get(url),
-          shouldRetryOnError: false,
-        }}
-      >
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </SWRConfig>
-    </Provider>
+    <SWRConfig
+      value={{
+        fetcher: (url) => axiosClient.get(url),
+        shouldRetryOnError: false,
+      }}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SWRConfig>
   );
 }
